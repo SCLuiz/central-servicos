d58441a25976be82c62e5339013059086cdfbca0
SCLuiz <luiz.santos@openfinancebrasil.org.br>
2025-10-23 12:32:33 -0300

    Configuração OAuth 2.0

--- oauth-config.js
+++ oauth-config.js
@@ -11,8 +11,8 @@ const OAUTH_CONFIG = {
     // URL de callback (GitHub Pages)
     redirectUri: 'https://scluiz.github.io/central-servicos/oauth-callback.html',
 
-    // Scopes necessários para acessar Jira
-    scope: 'read:jira-user read:jira-work offline_access',
+    // Scopes necessários para acessar Jira (granulares)
+    scope: 'read:me read:account read:jira-user read:jira-work read:user:jira read:issue-worklog:jira read:issue-worklog.property:jira read:workflow:jira read:workflow-scheme:jira read:workflow.property:jira read:work-item-info:jira read:object:jira read:servicedesk-request manage:servicedesk-customer write:servicedesk-request read:servicemanagement-insight-objects offline_access',
 
     // Audience (Jira API)
     audience: 'api.atlassian.com',
